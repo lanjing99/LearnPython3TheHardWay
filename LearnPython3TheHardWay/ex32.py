2916ce0aa9b707c335034bf6525de6423a825aa7
wenjing <84336951@qq.com>
2020-08-16 20:05:39 +0800

    add ex32.py

---
+++ LearnPython3TheHardWay/ex32.py
@@ -0,0 +1,11 @@
+names = ["cat", "dog", "pig"]
+numbers = [12, 1, 2, 5]
+
+for name in names:
+    print(f"I am {name}")
+
+for number in numbers:
+    print(f"number is {number}")
+
+for i in range(1, 5):
+    print(f"i is {i}")
